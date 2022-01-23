e07c69214c5c8f4a3f7afac91e42abef4d454319
Shobraj Battu <shobraj.battu.contractor@pepsico.com>
2022-01-23 12:09:06 -0600

    This is to update week 2

---
+++ Week 2/Lecture_2.py
@@ -0,0 +1,43 @@
+# This is single line comment in the code
+
+"""
+    This is for multiple line comments in the code
+"""
+print("This is the first class")
+
+seriesNumber = 1 # This is for int
+floatNumber = 1.234 # This is for float
+stringName = "This is string" # This is string
+print(seriesNumber)
+print(floatNumber)
+print(stringName)
+
+# inputEntered = input("Enter some input")
+# print(inputEntered)
+# numberEntered = int(input("Enter a integer"))
+# print(numberEntered)
+# floatEntered = float(input("Enter a floating number"))
+# print(floatEntered)
+#this will give you an error
+# stringEntered = string(input("Enter a string"))
+# print(stringEntered)
+
+#math operations
+number_1 = 10
+number_2 = 2
+additionTwoNumber = number_1 + number_2
+subtractionTwoNumber = number_1 - number_2
+multiplcationTwoNumber = number_1 * number_2
+divisionTwoNumber = number_1 / number_2
+number_3 = 3
+integerDivistionTwoNumber = number_1 // number_3
+remainderTwoNumber = number_1 % number_3
+expontentialTwoNumber = number_1 ** number_2
+
+print(additionTwoNumber)
+print(subtractionTwoNumber)
+print(multiplcationTwoNumber)
+print(divisionTwoNumber)
+print(integerDivistionTwoNumber)
+print(remainderTwoNumber)
+print(expontentialTwoNumber)

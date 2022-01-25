af46c64cd2873180f88a793b8431374da1037d1c
kesireddyadarsh <shobraj.battu.contractor@pepsico.com>
2022-01-25 13:01:16 -0600

    Class end

--- Week 2/class_2.py
+++ Week 2/class_2.py
@@ -3,4 +3,28 @@
 """
     This is multiple line comment
 """
-print("Welcome to class")
+# print("Welcome to class")
+#
+# serialNumber = 20
+# personName = "Adarsh"
+# floatValue = 19.9
+# checkCondition = True
+#
+# age = int(input("Enter your age"))
+# print(age)
+
+"""
+This is first problem and using print we are printing outputs
+"""
+numberOne = 32
+numberTwo = 18
+resultOfDiv = numberOne // numberTwo
+
+"""
+Prob 2:  laskjdfalsjd
+Author: Adarsh Kesireddy
+Date: Jan 25th
+"""
+# print(resultOfDiv)
+resultOfExp = numberOne ** numberTwo
+print(resultOfExp)

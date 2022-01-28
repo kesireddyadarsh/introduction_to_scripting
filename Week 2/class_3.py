f45c76009b1976bedcfaa21d8c2715adab73479d
kesireddyadarsh <shobraj.battu.contractor@pepsico.com>
2022-01-28 01:19:28 -0600

    "Lecture 3"

---
+++ Week 2/class_3.py
@@ -0,0 +1,85 @@
+#Class 3
+
+# trueCheck_1 = True
+#
+# if trueCheck_1:
+#     print("Condition is true")
+
+# number_1 = 10
+# number_2 = 20
+#
+# if number_1 < number_2:
+#     print("Number_1 is higher value")
+#
+# print("End")
+
+# temp = float(input("Whats the weather"))
+#
+# if temp < 40:
+#     print("Its cold")
+# else:
+#     print("Its ok !")
+
+
+# name_1 = "Adarsh"
+# name_2 = "Adarsh"
+#
+# test_number_1 = 20
+# test_number_2 = 30
+#
+# if test_number_1 != test_number_2:
+#     print("In if Condition")
+# else:
+#     print("In else Condition")
+#
+# print("Outside the block")
+
+
+# inputNumber1 = int(input("First number"))
+# # inputNumber2 = int(input("Second number"))
+# print("first number ", inputNumber1)
+# # print("Second number ", inputNumber2)
+#
+# if not(inputNumber1 < 10):
+#     print("In the condition")
+
+
+
+# True or False:
+# same code
+#
+# if true:
+#     one code
+# else:
+#     second code
+#
+# if true_1:
+#     one code
+# elif true _2:
+#     second code
+# else:
+#     third code
+#
+#
+#
+# if true_1:
+#     one code
+# else:
+#     if true_2:
+#         second code
+#     else:
+#         third code
+
+
+# number [0, 10]
+#number (10, 100]
+#nnumber (100, inf]
+
+number_1 = int(input("Ask for the number"))
+
+if number_1 <= 10 and number_1 >= 0:
+     print("Between 0 to 10")
+elif number_1 >10 and number_1 <= 100:
+    print("Between 10 and 100")
+else:
+    print("Greater than 100")

c5c6eacee202bd0d65e39dd22bcf8164de8bdb57
kesireddyadarsh <shobraj.battu.contractor@pepsico.com>
2022-02-10 12:18:43 -0600

    Objects class 2

---
+++ Week 4/class_7.py
@@ -0,0 +1,64 @@
+# print("This is class 7")
+
+# def factorial(N):
+#     if N == 1:
+#         return 1
+#     else:
+#         return N*factorial(N-1)
+#
+# N = 7
+# print(factorial(7))
+
+# number  = 10/20*2 #1
+# number_1 = 10/(20*2) #2
+# print(number,number_1)
+
+# class car():
+#     def __init__(self, color, mileage):
+#         self.color = color
+#         self.mileage = mileage
+#
+#     def __str__(self):
+#         return 'a {self.color} '.format(self=self)
+#
+# my_car = car("red",12)
+# print(my_car)
+# my_car.color = "green"
+# print(my_car)
+
+
+# class car():
+#     def __init__(self, color):
+#         self.color = color
+#         print(self.__color)
+#
+#     def set_color(self,color):
+#         self.__color = color
+#         print(self.__color)
+#
+#     def get_color(self):
+#         return self.__color
+#
+# my_car = car("red")
+# my_car.color = "green"
+# my_car.set_color("green")
+
+# class Person(object):
+#     def __init__(self, name):
+#         self.name = name
+#
+#
+#     def isEmployed(self):
+#         return False
+#
+# class Employee(Person):
+#     def isEmployed(self):
+#         print("Here")
+#     # def __init__(self, arg):
+#     #     super(Employee, self).__init__()
+#     #     self.arg = arg
+# empl_1 = Person("Adarsh")
+# empl_2 = Person("Ram")
+
+# print(10)
+# print("String")

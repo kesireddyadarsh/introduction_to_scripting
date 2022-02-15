0e079b9cd4469931b843f267c88df071c027f6e5
kesireddyadarsh <shobraj.battu.contractor@pepsico.com>
2022-02-15 11:51:10 -0600

    List

---
+++ Week 5/Lecture_8.py
@@ -0,0 +1,17 @@
+#project
+print("Class 8")
+lst_numbers = [i for i in range(0,50)]
+print(lst_numbers)
+
+repeating_list = [1]*5
+# print(repeating_list)
+
+string_list = ["class"]*10
+# print(string_list)
+
+cat_list = repeating_list + string_list
+# print(cat_list)
+
+days = ["Sun","MOn","Tue","Wed","Thr","Fir","Sat"]
+working_days = days[1:6]
+print(working_days)

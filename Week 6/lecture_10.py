4d05c612a0ba1be07ee3c79f6c3e1c2a19ad9daf
kesireddyadarsh <shobraj.battu.contractor@pepsico.com>
2022-02-22 13:29:04 -0600

    lecture 10

---
+++ Week 6/lecture_10.py
@@ -0,0 +1,80 @@
+
+# temp = "this is string"
+#
+# # temp[0] = temp[0].upper()
+# # print(temp)
+# # temp_2 =""
+# # for ch in range(0,len(temp)):
+# #     if ch == 0:
+# #         temp_2 += temp[ch].upper()
+# #     else:
+# #         temp_2 += temp[ch]
+# # print(temp_2)
+#
+# #slice from 'his is string' store
+# #convert t to T
+# #string_concatenation
+#
+# temp_slice = temp[1 : len(temp)]
+# upper_case = temp[0].upper()
+# temp_cont = upper_case+ temp_slice
+
+
+
+# print(temp[3])
+
+# temp = int(input('enter a number '))
+# print(temp)
+
+# name = "Test"
+# for ch in name:
+#     print(ch)
+
+# def main():
+#     userInput = input("Enter a string")
+#     print('This is input:::: ',userInput)
+#     count = 0
+#     for ch in userInput:
+#         if ch == 'T' or ch == 't':
+#             count += 1
+#     print(count)
+#
+# main()
+
+# string_1 = "String one"#10
+# string_2 = "String two"
+# string_concatenation = string_1 +" "+ string_2
+# print(string_concatenation)
+# print(len(string_concatenation))
+# print(string_1[7:len(string_1)]+" "+string_2[7:len(string_2)])
+
+# string_1 = "Test in"
+# if "Test" in string_1:
+#     print("In true condition")
+
+# string_name = 'Forclass12312'
+# # bool_alnum = string_name.isalnum()
+# # print(bool_alnum)
+#
+# bool_alpha = string_name.isalpha()
+# print(bool_alpha)
+#
+# bool_digit = string_name.isdigit()
+# print(bool_digit)
+#
+# bool_lower = string_name.islower()
+# print(bool_lower)
+#
+# bool_space = string_name.isspace()
+# print(bool_space)
+#
+# bool_upper = string_name.isupper()
+# print(bool_upper)
+
+# string_name =" Test me with all "
+# print(string_name.lower())
+# print(string_name.lstrip())
+# print(string_name.lstrip(' Test'))
+# print(string_name.strip())
+# print(string_name.rstrip())
+# print(string_name.upper())
